28482587424a7e0ad518028970f4b1d790c02f29
gachouchani1999 <gachouchani@ndu.edu.lb>
2022-09-29 16:34:55 +0300

    added keypair

--- utils/account.js
+++ utils/account.js
@@ -14,12 +14,17 @@ const nearAPI = require("near-api-js");
  * @since 0.0.1
 
 */
-function createLockedAccount()  {
+async function createLockedAccount()  {
     // Connects to near via config
     const near = await nearAPI.connect(config);
+    // Creates a key pair with full access that needs to be saved in the database
     const keyPair = nearAPI.KeyPair.fromRandom("ed25519");
+    // Finds the public key of the keypair and creates 
     const publicKey = keyPair.getPublicKey().toString();
     const accountID = nearAPI.utils.PublicKey.fromString(publicKey).data.hexSlice();
-    
+    const account = await near.account(accountID);
+    account.addKey(
+
+    )
     return [accountID, keyPair]
 }
