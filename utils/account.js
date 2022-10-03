811a4f8dd37e690343341d25747f99974f030fc9
gachouchani1999 <gachouchani@ndu.edu.lb>
2022-10-03 13:25:04 +0300

    added extra functionality to locked account creation function

--- utils/account.js
+++ utils/account.js
@@ -14,17 +14,41 @@ const nearAPI = require("near-api-js");
  * @since 0.0.1
 
 */
-async function createLockedAccount()  {
+async function createLockedAccount(nftAddress, tokenId)  {
     // Connects to near via config
     const near = await nearAPI.connect(config);
     // Creates a key pair with full access that needs to be saved in the database
     const keyPair = nearAPI.KeyPair.fromRandom("ed25519");
-    // Finds the public key of the keypair and creates 
+    // Finds the public key of the keypair and creates a new key pair that would be visible to the user privately
+    // This key pair would be responsible for the user to connect but does not have any functionality other than that. 
+    // The other key pair would be deleted after giving approval of the NFT to the user
     const publicKey = keyPair.getPublicKey().toString();
     const accountID = nearAPI.utils.PublicKey.fromString(publicKey).data.hexSlice();
     const account = await near.account(accountID);
+    const locked_keyPair = nearAPI.KeyPair.fromRandom("ed25519");
+    const locked_publicKey = locked_keyPair.getPublicKey().toString();
     account.addKey(
-
+      locked_publicKey,
+      "test-locked.testnet",
+      null
     )
-    return [accountID, keyPair]
+    const contract = new nearAPI.Contract(
+      account,
+      nftAddress,
+        {
+          changeMethods: ["nft_approve"],
+          sender : account,
+        }
+    );
+    // giving approval to the Copy NFT contract to be able to transfer the locked token to the required address at repossession or claim
+    const result = await contract.nft_approve(
+      {
+        token_id: tokenId,
+        account_id: "test-locked.nft",
+      }
+    );
+    // Ensure that the result is successful before moving on
+    
+    
+    return [accountID, locked_keyPair]
 }
